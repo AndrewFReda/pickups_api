0ac4d555c35d96b7514ebfb8617c8d0bcdbbc306
Andrew Reda <andrewfreda@gmail.com>
2014-03-30 14:31:58 -0400

    Initial project config tasks   * use postgres   * edit gems   * add Pry   * config to API instead of rails app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ PickupsApi::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  require 'pry'
+  IRB = Pry
 end
