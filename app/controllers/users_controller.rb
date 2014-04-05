f3023d296ad8c394ec9d61c37ee4d4b05c3c2ebb
Andrew Reda <andrewfreda@gmail.com>
2014-04-05 15:05:13 -0400

    add show endpoint for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
 
-    def create
+  def create
     @user = User.new user_params
     if @user.save
       render 'show'
     else
       render json: { error: 'Could not create user.' }.to_json, status: 400
     end
+  end
+
+  def show
+    @user = User.find params[:uid]
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:uid, :provider, :name, :oauth_token, :oauth_expires_at, :player_id)
   end
 
 
